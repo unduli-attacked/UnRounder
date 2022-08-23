0bcd358fa53088d09edc9ef46dd863fa65085a9e
Jocelyn McHugo <45951442+unduli-attacked@users.noreply.github.com>
2022-08-23 12:59:18 -0600

    add js

--- content.js
+++ content.js
@@ -1,1 +1,9 @@
-console.log("UnRounder loaded!");
+document.body.onload = function(){
+	var allElements = document.body.getElementsByTagName('*');
+
+	for (var i=0; i < allElements.length; i++){
+		allElements[i].style.borderRadius="0";
+	}
+
+	console.log("UnRounder loaded!");
+};
